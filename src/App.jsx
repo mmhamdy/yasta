93144c9a9ebb2d8b9756c1cfef26911f5536ea09
mmhamdy <mohamdy2772@gmail.com>
2022-03-14 20:42:20 +0200

    finished styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,47 +5,89 @@ const App = () => {
   return (
     <div className="app">
       <div className="header">
-        <div><BsCardChecklist className="header__icon"/></div>
-        <blockquote className="header__quote" cite="https://www.goodreads.com/quotes/340402-don-t-put-off-until-tomorrow-what-you-can-do-today">
-          <p>Don't put off until tomorrow what you can do today.</p>
-        </blockquote>
+        <div className="header__icon-container">
+          <BsCardChecklist className="header__icon"/>
+        </div>
+        <div className="header__quote-container">
+          <blockquote className="header__quote" cite="https://www.goodreads.com/quotes/340402-don-t-put-off-until-tomorrow-what-you-can-do-today">
+            <p>Don't put off until tomorrow what you can do today.</p>
+          </blockquote>
+        </div>
       </div>
       <div className="form">
         <form>
-          <input 
-            type="text"
-            name="text"
-            placeholder="So, what needs to be done?"
-          />
-          <div>
-            <button type="submit"><BsPlus className="form__icon"/></button>
+          <div className="form__container">
+            <div className="form__input-container">
+              <input 
+                type="text"
+                name="text"
+                className="form__input"
+                placeholder="So, what needs to be done?"
+              />
+            </div>
+            <div className="form__btn-container">
+              <button className="form__submit-btn" type="submit">
+                <BsPlus className="form__add-icon"/>
+              </button>
+            </div>
           </div>
         </form>
       </div>
       <div className="filters">
-        <p>Show:</p>
-        <div className="btn-group filter-btns">
+        <div className="filters__text">
+          <p>Show:</p>
+        </div>
+        <div className="btn-group filters__btns">
           <button type="button">all</button>
           <button type="button">active</button>
           <button type="button">done</button>
         </div>
       </div>
       <div className="task-count">
-        <h3>3 tasks remaining</h3>
+        <p className="task-count__text">&#9201; 3 tasks remaining</p>
       </div>
       <div className="todos">
-        <ul className="todo-list">
-          <li className="todo">
-            <div>
-              <input type="checkbox" id="todo-0" defaultChecked={true}/>
-              <label htmlFor="todo-0">Check your Todos</label>
+        <ul className="todos__todo-list">
+          <li className="todos__todo-item">
+            <div className="todos__input">
+              <label className="todos__label" htmlFor="todo-0">First task
+                <input className="checkbox" type="checkbox" id="todo-0" defaultChecked={false}/>
+                <span className="checkmark"></span>
+              </label>
             </div>
-            <div className="btn-group control-btns">
-              <button type="button"><BsPencil /></button>
-              <button type="button"><BsTrash /></button>
+            <div className="btn-group todos__control-btns">
+              <button className="todo-item__edit-btn" type="button"><BsPencil /></button>
+              <button className="todo-item__remove-btn" type="button"><BsTrash /></button>
+            </div>
+          </li>
+          <li className="todos__todo-item">
+            <div className="todos__input">
+              <label className="todos__label" htmlFor="todo-1">Second task
+                <input className="checkbox" type="checkbox" id="todo-1" defaultChecked={false}/>
+                <span className="checkmark"></span>
+              </label>
+            </div>
+            <div className="btn-group todos__control-btns">
+              <button className="todo-item__edit-btn" type="button"><BsPencil /></button>
+              <button className="todo-item__remove-btn" type="button"><BsTrash /></button>
+            </div>
+          </li>
+          <li className="todos__todo-item">
+            <div className="todos__input">
+              <label className="todos__label" htmlFor="todo-2">Third task
+                <input className="checkbox" type="checkbox" id="todo-2" defaultChecked={false}/>
+                <span className="checkmark"></span>
+              </label>
+            </div>
+            <div className="btn-group todos__control-btns">
+              <button className="todo-item__edit-btn" type="button"><BsPencil /></button>
+              <button className="todo-item__remove-btn" type="button"><BsTrash /></button>
             </div>
           </li>
         </ul>
+      </div>
+      <div className="footer">
+        <p className="footer__text">Made with React</p>
       </div>
     </div>
   );
