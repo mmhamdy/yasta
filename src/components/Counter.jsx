76ae6920598b4ec7a8987d54bcc7f7bdb6f73a06
mmhamdy <mohamdy2772@gmail.com>
2022-03-21 01:29:53 +0200

    replace emoji with svg icon in Counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,12 @@
+import { BsStopwatch } from 'react-icons/bs';
+
 const Counter = ({ tasks }) => {
   const tasksNoun = tasks.length !== 1 ? "tasks" : "task"
   const tasksCount = `${tasks.length} ${tasksNoun} remaining`
   return (
     <div className="task-count">
       <p className="task-count__text">
-        &#9201; {tasksCount}
+        <BsStopwatch /> {tasksCount}
       </p>
     </div>
   )
